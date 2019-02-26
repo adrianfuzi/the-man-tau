9272ff6d8777b580d9777efc9adece009409a80a
adewfalcon <37154180+adewfalcon@users.noreply.github.com>
2019-02-26 19:28:31 +0100

    Initial commit

---
+++ javascript/smoothScroll.js
@@ -0,0 +1,23 @@
+$(".music").click(function() {
+    $('html, body').animate({
+        scrollTop: $("#music").offset().top-180
+    }, 500);
+});
+
+$('.events').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#events").offset().top-180
+    }, 500);
+});
+
+$('.about').click(function() {
+    $('html, body').animate({
+        scrollTop: $("#about").offset().top-180
+    }, 500);
+});
+
+$('#title').click(function(event) {
+    event.preventDefault();
+    
+    $('html, body').animate({scrollTop: 0}, 0);
+});
