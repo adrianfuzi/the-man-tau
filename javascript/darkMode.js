9230a6594975b364d5079f9db0754df493b0c3a9
adrianfuzi <37154180+adewfalcon@users.noreply.github.com>
2021-09-16 13:02:20 +0200

    Update content
    - Add new album
    - Update "about" section
    - Add better shadow
    - Re-enable hero video
    - Clean up code

--- javascript/darkMode.js
+++ javascript/darkMode.js
@@ -1,18 +1,18 @@
-var checkbox = document.querySelector('input[name=theme]');
+var checkbox = document.querySelector("input[name=theme]");
 
-checkbox.addEventListener('change', function() {
-    if (this.checked) {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'dark');
-    } else {
-        trans()
-        document.documentElement.setAttribute('data-theme', 'light');
-    }
+checkbox.addEventListener("change", function () {
+  if (this.checked) {
+    trans();
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    trans();
+    document.documentElement.setAttribute("data-theme", "light");
+  }
 });
 
 let trans = () => {
-    document.documentElement.classList.add('transition');
-    window.setTimeout(() => {
-        document.documentElement.classList.remove('transition');
-    }, 1000);
-}
+  document.documentElement.classList.add("transition");
+  window.setTimeout(() => {
+    document.documentElement.classList.remove("transition");
+  }, 1000);
+};
--- javascript/heroVideo.js
+++ javascript/heroVideo.js
@@ -1,41 +1,49 @@
 // IFrame
 
 // Load the IFrame Player API code asynchronously.
-var tag = document.createElement('script');
+var tag = document.createElement("script");
 tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
+var firstScriptTag = document.getElementsByTagName("script")[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 // Replace the 'ytplayer' element with an <iframe> and
 // YouTube player after the API code downloads.
 var player;
 
-if ( $(window).width() >= 1158) {
+if ($(window).width() >= 1158) {
   function onYouTubePlayerAPIReady() {
-    player = new YT.Player('ytplayer', {
-      height: '360',
-      width: '640',
-      playerVars: { 'autoplay': 1, 'controls': 0,'disablekb':1,'enablejsapi':1, 'modestbranding':1, 'rel': 0, 'mute': 1},
-      videoId: 'D-1Sp3z3Kqw'
+    player = new YT.Player("ytplayer", {
+      height: "360",
+      width: "640",
+      playerVars: {
+        autoplay: 0,
+        controls: 0,
+        disablekb: 1,
+        enablejsapi: 1,
+        modestbranding: 1,
+        rel: 0,
+        mute: 1,
+      },
+      videoId: "nU2Rxkwx8DQ",
     });
   }
 }
 
 // Mute-Unmute Button
 
-const soundOff = document.querySelector('.nav__list--sound-off');
-const soundOn = document.querySelector('.nav__list--sound-on');
+const soundOff = document.querySelector(".nav__list--sound-off");
+const soundOn = document.querySelector(".nav__list--sound-on");
 
 function musicOn() {
-    soundOff.style.display = 'none';
-    soundOn.style.display = 'block';
-    player.unMute();
+  soundOff.style.display = "none";
+  soundOn.style.display = "block";
+  player.unMute();
 }
 
 function musicOff() {
-    soundOff.style.display = 'block';
-    soundOn.style.display = 'none';
-    player.mute();
+  soundOff.style.display = "block";
+  soundOn.style.display = "none";
+  player.mute();
 }
 
-soundOff.addEventListener('click', musicOn);
-soundOn.addEventListener('click', musicOff);
+soundOff.addEventListener("click", musicOn);
+soundOn.addEventListener("click", musicOff);
